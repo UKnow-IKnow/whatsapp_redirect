c2c04ed23543e846ceb4c11a5d28ab6a7cf25625
UKnow-IKnow <sarojsahu014@gmail.com>
2022-07-20 15:28:18 +0530

    done

--- app/src/main/java/com/example/whatsapp_redirect/MainActivity.kt
+++ app/src/main/java/com/example/whatsapp_redirect/MainActivity.kt
@@ -1,11 +1,36 @@
 package com.example.whatsapp_redirect
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.whatsapp_redirect.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.wpBtn.setOnClickListener {
+            val uri = Uri.parse("https://wa.me/message/U3AQWIPMBB6QO1")
+
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.setPackage("com.whatsapp")
+            intent.data = uri
+            if (intent.resolveActivity(this.packageManager) != null) {
+                startActivity(intent)
+            } else {
+                Toast.makeText(
+                    this,
+                    "Looks like you don't have WhatsApp installed!",
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+        }
     }
 }
